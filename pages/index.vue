14fefed43442cdaf68680e2da91e8715593f06ae
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-10-31 23:48:33 +0200

    Created the main section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>home page</h1>
+  <section class="container flex justify-between space-x-10 mt-32">
+    <div class="space-y-8 pt-32">
+      <h4 class="uppercase text-xs">fresh fruit for you</h4>
+      <h1 class="uppercase text-Green text-6xl font-bold leading-14">
+        Eat as<br />
+        you want
+      </h1>
+      <p class="max-w-xl text-Black opacity-70">
+        Fruit and vegetables should be an important part of your daily diet. They are naturally good
+        and contain vitamins and minerals that can help to keep you healthy.
+      </p>
+      <div class="space-x-10">
+        <button
+          class="bg-Green text-White hover:bg-White hover:text-Green border-solid border-2 border-Green rounded-tr-large rounded-bl-large capitalize px-3 py-1"
+        >
+          add to cart
+        </button>
+        <button
+          class="bg-White text-Green hover:bg-Green hover:text-White border-solid border-2 border-Green rounded-tr-large rounded-bl-large capitalize px-3 py-1"
+        >
+          learn more
+        </button>
+      </div>
+    </div>
+    <div><img src="/images/main_product.png" alt="Basket with fruits" /></div>
+  </section>
 </template>
