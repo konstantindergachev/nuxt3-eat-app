f6e3cf5d0b9ce4ea1188be6b687bd105034fff35
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-12-15 15:28:24 +0200

    Refactored the navigation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,28 +1,38 @@
 <template>
   <nav class="text-Black flex justify-between space-x-10 capitalize">
     <NuxtLink
-      to="/location"
+      v-for="nav in navigation"
+      :key="nav.id"
+      :to="nav.path"
       activeClass="border-b-2 border-b-Green"
       class="border-transparent border-b-2 hover:border-b-Green"
-      >location</NuxtLink
-    >
-    <NuxtLink
-      to="/fruits"
-      activeClass="border-b-2 border-b-Green"
-      class="border-transparent border-b-2 hover:border-b-Green"
-      >fruits</NuxtLink
-    >
-    <NuxtLink
-      to="/review"
-      activeClass="border-b-2 border-b-Green"
-      class="border-transparent border-b-2 hover:border-b-Green"
-      >review</NuxtLink
-    >
-    <NuxtLink
-      to="/about"
-      activeClass="border-b-2 border-b-Green"
-      class="border-transparent border-b-2 hover:border-b-Green"
-      >about us</NuxtLink
+      >{{ nav.title }}</NuxtLink
     >
   </nav>
 </template>
+<script setup lang="ts">
+import { INavigation } from '@/interfaces/navigation';
+
+const navigation: INavigation[] = [
+  {
+    id: '1',
+    path: '/fruits',
+    title: 'fruits',
+  },
+  {
+    id: '2',
+    path: '/review',
+    title: 'review',
+  },
+  {
+    id: '3',
+    path: '/about',
+    title: 'about',
+  },
+  {
+    id: '4',
+    path: '/location',
+    title: 'location',
+  },
+];
+</script>
