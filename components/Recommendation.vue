d130c9fdceeca9f3562b67b70859bdf754537032
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-12-19 09:28:25 +0200

    Created the recommendation component

---
+++ components/Recommendation.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="container flex flex-col lg:flex-row justify-between lg:space-x-10 mt-14 md:mt-32">
+    <div class="w-full lg:w-6/12">
+      <img src="/images/product11.png" alt="Product 11" />
+    </div>
+    <div class="w-full lg:w-6/12 space-y-8 pt-14 md:pt-32">
+      <div class="flex lg:space-x-4">
+        <h1 class="text-Black text-3xl font-bold leading-14 text-center md:text-left">
+          Eat a banana for a<br />
+          healthy snack
+        </h1>
+        <img src="/images/leath.png" alt="Leath" class="hidden md:block" />
+      </div>
+      <p class="lg:max-w-xl text-Black opacity-70 text-center md:text-left">
+        You should eat at least five serves of vegetables and two serves of fruit each day. Choose
+        different colours and varieties.
+      </p>
+      <div class="w-full">
+        <ul class="flex flex-wrap">
+          <li class="flex justify-between items-center mb-10 order-1 space-x-24 md:space-x-4">
+            <span
+              class="uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full py-1 w-11 text-center"
+              >1</span
+            >
+            <div>
+              <h3 class="uppercase text-Green text-6xl font-bold leading-14">150+</h3>
+              <p class="capitalize">global franchises</p>
+            </div>
+          </li>
+          <li class="flex justify-between items-center mb-10 order-3 space-x-24 md:space-x-4">
+            <span
+              class="uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full py-1 w-11 text-center"
+              >2</span
+            >
+            <div>
+              <h3 class="uppercase text-Green text-6xl font-bold leading-14">100%</h3>
+              <p class="capitalize">organic product</p>
+            </div>
+          </li>
+          <li class="flex justify-between items-center mb-10 order-2 space-x-24 md:space-x-4">
+            <span
+              class="block md:ml-12 uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full py-1 w-11 text-center"
+              >3</span
+            >
+            <div>
+              <h3 class="uppercase text-Green text-6xl font-bold leading-14">97%</h3>
+              <p class="capitalize">happy customer</p>
+            </div>
+          </li>
+          <li class="flex justify-between items-center mb-10 order-4 space-x-24 md:space-x-4">
+            <span
+              class="block md:ml-12 uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full py-1 w-11 text-center"
+              >4</span
+            >
+            <div>
+              <h3 class="uppercase text-Green text-6xl font-bold leading-14">10m+</h3>
+              <p class="capitalize">monthly sales</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
