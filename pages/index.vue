cfaae897edd95a49592c3b83d7bda4bd4c1276b1
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-12-19 09:30:02 +0200

    Created the delivery component

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,7 @@
 <template>
   <TheBanner />
   <Recommendation />
-  <section class="container flex flex-col lg:flex-row justify-between lg:space-x-10 mt-14 md:mt-32">
-    <div class="w-full mb-10 text-center lg:text-left lg:w-6/12">
-      <h1 class="text-Black text-3xl font-bold leading-14 mb-10">
-        Our purpose is to deliver<br />
-        fresh fruit to you
-      </h1>
-      <p class="lg:max-w-xl text-Black opacity-70">
-        Fruits and vegetables contain many vitamins and minerals that are good for your health.
-        These include vitamins
-      </p>
-    </div>
-    <div class="w-full lg:w-6/12">
-      <ul class="flex flex-col md:flex-row lg:flex-col">
-        <li class="flex items-center mb-10 order-1 space-x-2 md:space-x-4">
-          <span
-            class="uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full md:py-1 w-11 text-center"
-            >1</span
-          >
-          <div>
-            <h3 class="text-Black text-2xl font-bold leading-14">Order Your Fruit</h3>
-            <p class="text-Black opacity-70">A serve of fruit is about one medium piece</p>
-          </div>
-        </li>
-        <li class="flex items-center mb-10 order-3 space-x-2 md:space-x-4">
-          <span
-            class="uppercase text-Green text-3xl font-bold leading-14 shadow-md rounded-full md:py-1 w-11 text-center"
-            >2</span
-          >
-          <div>
-            <h3 class="text-Black text-2xl font-bold leading-14">Delivery and Pickup</h3>
-            <p class="text-Black opacity-70">A serve of vegetables is about one cup of raw</p>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </section>
+  <Delivery />
   <section class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-14 md:mt-20">
     <UICard v-for="prod in topThreeProducts" :key="prod.id">
       <UIImage :imgSrc="prod.img" :imgName="prod.name" />
