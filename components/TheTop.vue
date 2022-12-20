2d48cbc58bddaa014978519a5783b954f92c60bb
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-12-20 09:17:28 +0200

    Created the top three component

---
+++ components/TheTop.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-14 md:mt-20">
+    <UICard v-for="prod in topThreeProducts" :key="prod.id">
+      <UIImage :imgSrc="prod.img" :imgName="prod.name" />
+      <UITitle>{{ prod.name }}</UITitle>
+      <UIStar> &#9733; &#9733; &#9733; &#9733; &#9733; </UIStar>
+      <UISubtitle> {{ moneyFormat('en-US', 'USD', prod.price) }} kg </UISubtitle>
+      <UIText>{{ prod.description }}</UIText>
+      <UIButton
+        className="bg-Green text-White capitalize hover:bg-White hover:text-Green border-solid border-2 border-Green rounded-tr-large rounded-bl-large px-3 py-1 justify-self-center h-10"
+        >{{ 'buy now' }}</UIButton
+      >
+    </UICard>
+  </section>
+</template>
+<script setup>
+import { moneyFormat } from '@/utils';
+
+const { topThreeProducts } = defineProps(['topThreeProducts']);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,7 @@
   <TheBanner />
   <Recommendation />
   <Delivery />
-  <section class="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-14 md:mt-20">
-    <UICard v-for="prod in topThreeProducts" :key="prod.id">
-      <UIImage :imgSrc="prod.img" :imgName="prod.name" />
-      <UITitle>{{ prod.name }}</UITitle>
-      <UIStar> &#9733; &#9733; &#9733; &#9733; &#9733; </UIStar>
-      <UISubtitle> {{ moneyFormat('en-US', 'USD', prod.price) }} kg </UISubtitle>
-      <UIText>{{ prod.description }}</UIText>
-      <UIButton
-        className="bg-Green text-White capitalize hover:bg-White hover:text-Green border-solid border-2 border-Green rounded-tr-large rounded-bl-large px-3 py-1 justify-self-center h-10"
-        >{{ 'buy now' }}</UIButton
-      >
-    </UICard>
-  </section>
+  <TheTop :topThreeProducts="topThreeProducts" />
   <section
     class="container flex flex-col md:flex-row justify-around items-center md:space-x-10 mt-14 md:mt-32 mb-12 shadow-lg rounded-lg p-4"
   >
@@ -50,7 +38,6 @@
 <script setup lang="ts">
 import { IFruit } from '@/interfaces/fruits';
 import { INewsletter } from '@/interfaces/nesletter';
-import { moneyFormat } from '@/utils';
 
 const { data } = await useLazyFetch<IFruit[]>('/api/fruits');
 const topThreeProducts = data.value?.filter((product, idx) => idx <= 2 && product);
