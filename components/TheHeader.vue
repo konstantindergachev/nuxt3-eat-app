70638a2df9accb0385cf6049a18a105e60cc8b01
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2022-12-30 09:58:06 +0200

    Added the basket icon. Fixed the menu.

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -4,14 +4,26 @@
       <img src="/images/logo.png" alt="logo" />
     </NuxtLink>
     <Navbar />
-    <UIButton
-      v-if="route.path !== '/signup'"
-      className="bg-Green text-White hover:bg-White hover:text-Green border-solid border-2 border-Green rounded-tr-large rounded-bl-large px-3 py-1"
-      path="/signup"
-      >{{ 'sign up' }}</UIButton
-    >
+    <div class="flex items-center relative">
+      <NuxtLink to="/basket" class="z-10"
+        ><img src="/images/carrybag.svg" alt="Carry bag" class="w-12"
+      /></NuxtLink>
+      <span class="absolute top-[1.20rem] left-[19px] text-red-500">{{ fruitsCount }}</span>
+      <UIButton
+        v-if="route.path !== '/signup'"
+        className="bg-Green text-White hover:bg-White hover:text-Green border-solid border-2 border-Green rounded-tr-large rounded-bl-large px-3 py-1"
+        path="/signup"
+        >{{ 'sign up' }}</UIButton
+      >
+    </div>
   </header>
 </template>
 <script setup>
+import { useStoreBasket } from '@/stores/basket';
 const route = useRoute();
+
+const storeBasket = useStoreBasket();
+const fruitsCount = computed(() => {
+  return storeBasket.getBasket.length;
+});
 </script>
