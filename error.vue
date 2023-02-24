2152a6d9dbc14bdef95701aa934eb3beee47780f
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-02-24 10:39:36 +0200

    Created the custom error page

---
+++ error.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="relative container mx-auto p-6 min-h-screen grid grid-cols-[1fr] grid-rows-[auto,1fr,auto]"
+  >
+    <Head>
+      <Title>EatApp - Error Page</Title>
+    </Head>
+    <div class="col-span-full">
+      <TheHeader />
+    </div>
+    <main class="main flex flex-col items-center justify-evenly">
+      <h1 class="text-center uppercase text-red-500 text-6xl font-bold leading-14 mb-10">
+        {{ '404 Page' }}
+      </h1>
+      <img class="w-32" src="/images/exclamation-mark.svg" alt="Sxclamation mark" />
+      <UIButton type="button" class="btn btn-shape" :onClick="goBack">{{ 'back' }}</UIButton>
+
+      <slot />
+    </main>
+    <div class="col-span-full">
+      <TheFooter />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const router = useRouter();
+const goBack = () => {
+  router.back(-1);
+};
+</script>
