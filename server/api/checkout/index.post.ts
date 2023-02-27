f3169c85a0285d7b653346e06e0f18ace205a0ba
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-02-27 11:47:34 +0200

    Saved the order and order details data to the supabase db

--- server/api/checkout/index.post.ts
+++ server/api/checkout/index.post.ts
@@ -1,4 +1,41 @@
+import { supabase } from '@/client';
+import { ICheckoutOrder } from '@/interfaces/checkout';
+import { IBasket } from '@/interfaces/fruits';
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  const body: ICheckoutOrder = await readBody(event);
+
+  const { data: userData } = await supabase.from('customers').select('id').single();
+  if (!userData) {
+    return `This customer doesn't exist!`;
+  }
+
+  const { data } = await supabase
+    .from('orders')
+    .insert([
+      {
+        customer_id: userData.id,
+        ship_fullname: `${body.info.firstname} ${body.info.lastname}`,
+        ship_phone: body.info.phone,
+        ship_address: body.info.deliveryAddress,
+        ship_city: body.info.deliveryCity,
+        ship_country: body.info.deliveryCountry,
+      },
+    ])
+    .select('id');
+  const order = data && data[0];
+
+  body.basket.products;
+  body.basket.products.forEach(async (product: IBasket) => {
+    await supabase.from('order_details').insert([
+      {
+        order_id: order?.id,
+        fruit_id: product.id,
+        price: product.price,
+        quantity: product.count,
+      },
+    ]);
+  });
+
   return { body };
 });
