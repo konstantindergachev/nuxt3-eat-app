508a340ea557c4a32b69b6549e4b90438a082741
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-02-28 15:21:12 +0200

    Got the order data from the supabase db

--- server/api/orders/index.get.ts
+++ server/api/orders/index.get.ts
@@ -1,6 +1,59 @@
-import { orders } from '@/stub/orders';
 import { IOrder } from '@/interfaces/orders';
+import { supabase } from '@/client';
 
-export default defineEventHandler((event): IOrder[] => {
-  return orders;
+export default defineEventHandler(async (event): Promise<IOrder[]> => {
+  const _order = await supabase
+    .from('order_details')
+    .select(`order_id, price, quantity, fruits(id, name), orders(*)`);
+
+  const products = getProductsFromRawOrder(_order.data);
+  const orders = getOrders(_order.data, products);
+  return response(orders);
 });
+
+const getProductsFromRawOrder = (rawOrder: any) => {
+  return rawOrder.map((item: any) => {
+    return {
+      id: item.fruits.id,
+      title: item.fruits.name,
+      price: item.price,
+      count: item.quantity,
+      orderId: item.order_id,
+    };
+  });
+};
+
+const getOrders = (rawOrder: any, products: any) => {
+  const _rawOrder = rawOrder.reduce((obj: any, curr: any) => {
+    curr.fruits = products.filter((product: any) => {
+      if (product.orderId === curr.order_id) {
+        return product;
+      }
+    });
+    obj[`${curr.order_id}`] = curr;
+    return obj;
+  }, []);
+
+  return Object.values(_rawOrder);
+};
+
+const response = (rawOrder: any) => {
+  return rawOrder.map((item: any) => {
+    return {
+      id: item.order_id,
+      created_at: item.orders.order_date,
+      total: item.fruits.reduce((acc: any, curr: any) => {
+        return (acc += curr.price * curr.count);
+      }, 0),
+      products: item.fruits,
+      shipment: {
+        fullname: item.orders.ship_fullname,
+        phone: item.orders.ship_phone,
+        country: item.orders.ship_country,
+        city: item.orders.ship_city,
+        address: item.orders.ship_address,
+      },
+      user: { id: item.orders.customer_id },
+    };
+  });
+};
