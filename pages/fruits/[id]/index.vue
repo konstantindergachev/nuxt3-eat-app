57179cd91808e135a80660b8c319229cebd1c0b5
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-03-28 10:46:49 +0300

    Showed the favourite button only for sign in customers

--- pages/fruits/[id]/index.vue
+++ pages/fruits/[id]/index.vue
@@ -14,12 +14,14 @@
           Price:
           <span class="opacity-70">{{ moneyFormat('en-US', 'USD', fruit.price!) }}</span>
         </p>
-        <button type="button" v-if="fruit.popular" class="text-2xl">
-          <span class="text-red-500">&#128153;</span>
-        </button>
-        <UIButton type="button" v-else class="text-2xl" :onClick="addToFavorite">
-          <span class="opacity-70">&#9825;</span>
-        </UIButton>
+        <div v-if="auth.isAuthenticated">
+          <button type="button" v-if="fruit.popular" class="text-2xl">
+            <span class="text-red-500">&#128153;</span>
+          </button>
+          <UIButton type="button" v-else class="text-2xl" :onClick="addToFavorite">
+            <span class="opacity-70">&#9825;</span>
+          </UIButton>
+        </div>
       </div>
       <UIButton type="button" class="btn btn-shape justify-self-center" :onClick="addToBasket">{{
         'add to basket'
@@ -38,6 +40,8 @@ const route = useRoute();
 
 const { data } = await useFetch<IFruit>(() => `/api/fruit?fruitId=${route.params.id}`);
 let fruit: IFruit = reactive({ ...data.value });
+
+const auth = useAuth();
 
 const storeBasket = useStoreBasket();
 const addToBasket = () => {
