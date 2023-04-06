f780be84bcdc105a068897538f0ce94dc4d46b97
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-04-06 09:36:49 +0300

    Removed unused component

--- app.vue
+++ app.vue
@@ -1,18 +1,5 @@
 <template>
-  <NuxtLayout :name="layout">
+  <NuxtLayout>
     <NuxtPage :key="$route.fullPath" />
-    <a href="#" @click="view('home')">Home</a>
-    <a href="#" @click="view('counter')">Counter</a>
-    <LazyHomeWelcome v-if="page === 'home'" />
-    <LazyCounter v-if="page === 'counter'" />
   </NuxtLayout>
 </template>
-
-<script setup>
-// const MyComponent = shallowRef(resolveComponent('Counter'));
-const page = ref('home');
-
-const view = (name) => {
-  page.value = name;
-};
-</script>
--- pages/reviews/index.vue
+++ pages/reviews/index.vue
@@ -5,10 +5,15 @@
     </Head>
     <Carousel v-if="slides" :slides="slides" :interval="3000" controls indicators />
   </div>
+  <a href="#" @click="view('counter')">Counter</a>
+  <LazyCounter v-if="page === 'counter'" />
 </template>
 <script setup lang="ts">
 import { ISlide } from '@/interfaces/review';
 
 const { data } = await useFetch<ISlide[]>('/api/review');
 const slides = data.value;
+
+const page = ref('');
+const view = (name: string) => (page.value = name);
 </script>
