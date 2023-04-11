7096ae7ad48bd5cfd0553566c590198dd5225b9c
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-04-11 09:14:48 +0300

    Showed the post form only for auth user

--- pages/reviews/index.vue
+++ pages/reviews/index.vue
@@ -5,9 +5,13 @@
     </Head>
     <Carousel v-if="slides" :slides="slides" :interval="3000" controls indicators />
   </div>
-  <UIButton type="button" class="btn self-start mb-5" :onClick="() => view('post')">{{
-    'create post'
-  }}</UIButton>
+  <UIButton
+    v-if="auth.isAuthenticated"
+    type="button"
+    class="btn self-start mb-5"
+    :onClick="() => view('post')"
+    >{{ 'create post' }}</UIButton
+  >
   <LazyPost v-if="page === 'post'" />
 </template>
 <script setup lang="ts">
@@ -15,6 +19,7 @@ import { ISlide } from '@/interfaces/review';
 
 const { data } = await useFetch<ISlide[]>('/api/review');
 const slides = data.value;
+const auth = useAuth();
 
 const page = ref('');
 const view = (name: string) => (page.value = name);
