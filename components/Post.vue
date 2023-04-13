360af43a1688bab42c899b2c0ac131c3e9e71d1d
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-04-13 10:00:00 +0300

    Created the post component

---
+++ components/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="flex flex-wrap justify-between" :class="[page === 'post' ? 'lg:w-1/3' : 'lg:w-full']">
+    <li v-for="post in posts" :key="post.id" class="space-y-4 mb-8">
+      <h3 class="uppercase text-Green font-bold leading-14 text-3xl lg:text-3xl">
+        {{ post.title }}
+      </h3>
+      <p class="text-Black opacity-70">{{ post.text }}</p>
+      <h4 class="uppercase text-xs">{{ post.fullname }}</h4>
+      <h4 class="uppercase text-xs">{{ post.created_at.split('.')[0] }}</h4>
+    </li>
+  </ul>
+</template>
+
+>
+<script setup lang="ts">
+const { page, posts } = defineProps(['page', 'posts']);
+</script>
--- pages/reviews/index.vue
+++ pages/reviews/index.vue
@@ -14,19 +14,7 @@
   >
   <div class="flex flex-col md:flex-row md:justify-between">
     <LazyPostForm v-if="page === 'post'" />
-    <ul
-      class="flex flex-wrap justify-between"
-      :class="[page === 'post' ? 'lg:w-1/3' : 'lg:w-full']"
-    >
-      <li v-for="post in posts" :key="post.id" class="space-y-4 mb-8">
-        <h3 class="uppercase text-Green font-bold leading-14 text-3xl lg:text-3xl">
-          {{ post.title }}
-        </h3>
-        <p class="text-Black opacity-70">{{ post.text }}</p>
-        <h4 class="uppercase text-xs">{{ post.fullname }}</h4>
-        <h4 class="uppercase text-xs">{{ post.created_at.split('.')[0] }}</h4>
-      </li>
-    </ul>
+    <Post :page="page" :posts="posts" />
   </div>
 </template>
 <script setup lang="ts">
