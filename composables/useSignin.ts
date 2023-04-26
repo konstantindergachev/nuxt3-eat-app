bec06dc9858e140fc9a47ff5e7cddc4dc2d7cd06
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-04-26 09:49:51 +0300

    Created the sign in form abstraction

---
+++ composables/useSignin.ts
@@ -0,0 +1,90 @@
+import { ISignin, ISigninErrors } from '@/interfaces/signin';
+import Input from '@/components/UI/Input.vue';
+import { useStoreProfile } from '@/stores/profile';
+import { signinSchema } from '@/validation/signin.validation';
+
+export const useSignin = () => {
+  const form = reactive<ISignin>({
+    email: '',
+    password: '',
+  });
+
+  const errors = reactive<ISigninErrors>({
+    email: '',
+    password: '',
+    request: '',
+  });
+
+  const inputs = [
+    {
+      type: 'email',
+      name: 'email',
+      placeholder: 'test@test.com',
+      getValue: computed(() => form.email),
+      setValue: (value: string) => {
+        form.email = value;
+      },
+      component: Input,
+      error: computed(() => errors.email),
+    },
+    {
+      type: 'password',
+      name: 'password',
+      placeholder: '******',
+      getValue: computed(() => form.password),
+      setValue: (value: string) => {
+        form.password = value;
+      },
+      component: Input,
+      error: computed(() => errors.password),
+    },
+  ];
+
+  const validate = async (field: keyof ISigninErrors) => {
+    try {
+      await signinSchema.validateAt(field, form);
+      errors[field] = '';
+    } catch (error) {
+      if (error instanceof Error) {
+        errors[field] = error.message;
+      } else {
+        errors[field] = 'Unexpected error';
+      }
+    }
+  };
+
+  const storeProfile = useStoreProfile();
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    try {
+      const { id, firstname, lastname, email } = await $fetch('/api/auth/signin', {
+        method: 'post',
+        body: form,
+      });
+
+      const auth = useAuth();
+      auth.value.isAuthenticated = Boolean(id);
+
+      if (auth.value.isAuthenticated) {
+        const profile = { fullname: `${firstname} ${lastname}`, email, location: '' };
+        storeProfile.addToProfile(profile);
+        router.push('/profile');
+      }
+
+      form.email = '';
+      form.password = '';
+    } catch (error) {
+      if (error instanceof Error) {
+        errors.request = `Sorry! You don't have any account yet.`;
+      }
+    }
+  };
+
+  return {
+    handleSubmit,
+    inputs,
+    validate,
+    errors,
+  };
+};
--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,84 +1,23 @@
 <template>
-  <h1 class="text-center mt-10 mb-10 uppercase">Sign in</h1>
-  <form @submit.prevent="handleSignup" class="grid max-w-xl mx-auto">
-    <p
-      class="text-red-500 capitalize"
-      v-if="!!errors.email || !!errors.password || !!errors.request"
-    >
-      {{ errors.email || errors.password || errors.request }}
+  <div class="mx-auto">
+    <h1 class="text-center my-10 uppercase">Sign in</h1>
+    <p class="text-Green capitalize" v-if="!!errors.request">
+      {{ errors.request }}
     </p>
-    <UIInput
-      v-for="input in inputs"
-      :key="input.name"
-      :type="input.type"
-      :name="input.name"
-      :placeholder="input.placeholder"
-      className="shadow-lg border rounded-lg p-2 mb-2"
-      :modelValue="input.value"
-      :onValidate="validate"
-      @update:modelValue="input.getValue"
+    <TheForm
+      :onSubmit="handleSubmit"
+      :inputs="inputs"
+      :validate="validate"
+      className="grid max-w-xl mb-10"
     />
-    <UIButton type="submit" class="btn rounded-md justify-self-center lg:ml-4">{{
-      'send'
-    }}</UIButton>
-    <span class="text-center mt-10"
+    <span class="text-center"
       >If you don't have an account, than
       <NuxtLink to="/signup" class="text-Green capitalize"> sign up </NuxtLink></span
     >
-  </form>
+  </div>
 </template>
 <script setup lang="ts">
-import { ISigninErrors } from '@/interfaces/signin';
-import { useStoreProfile } from '@/stores/profile';
-import { signinSchema } from '@/validation/signin.validation';
-import { useSigninFormConfigs } from '@/composables/useSigninFormConfig';
+import { useSignin } from '@/composables/useSignin';
 
-const { inputs, form } = useSigninFormConfigs();
-
-const errors = reactive<ISigninErrors>({
-  email: '',
-  password: '',
-  request: '',
-});
-
-const storeProfile = useStoreProfile();
-const router = useRouter();
-
-const validate = async (field: keyof ISigninErrors) => {
-  try {
-    await signinSchema.validateAt(field, form);
-    errors[field] = '';
-  } catch (error) {
-    if (error instanceof Error) {
-      errors[field] = error.message;
-    } else {
-      errors[field] = 'Unexpected error';
-    }
-  }
-};
-
-const handleSignup = async () => {
-  try {
-    const { id, firstname, lastname, email } = await $fetch('/api/auth/signin', {
-      method: 'post',
-      body: form,
-    });
-
-    const auth = useAuth();
-    auth.value.isAuthenticated = Boolean(id);
-
-    if (auth.value.isAuthenticated) {
-      const profile = { fullname: `${firstname} ${lastname}`, email, location: '' };
-      storeProfile.addToProfile(profile);
-      router.push('/profile');
-    }
-
-    form.email = '';
-    form.password = '';
-  } catch (error) {
-    if (error instanceof Error) {
-      errors.request = `Sorry! You don't have any account yet.`;
-    }
-  }
-};
+const { handleSubmit, inputs, validate, errors } = useSignin();
 </script>
