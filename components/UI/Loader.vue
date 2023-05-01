33b2229fc6f72332a9678df6ac8babe0a8691f56
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2023-05-01 11:42:27 +0300

    Created the loader

---
+++ components/UI/Loader.vue
@@ -0,0 +1,42 @@
+<template>
+  <span class="loader"></span>
+</template>
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border: 2px solid #59be29;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  -webkit-animation: rotation 1s linear infinite;
+  animation: rotation 1s linear infinite;
+}
+.loader:after {
+  content: '';
+  position: absolute;
+  left: 50%;
+  top: 0;
+  background: #ff3d00;
+  width: 3px;
+  height: 24px;
+  transform: translateX(-50%);
+}
+
+@-webkit-keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- pages/fruits/index.vue
+++ pages/fruits/index.vue
@@ -4,7 +4,7 @@
       <Title>EatApp - Fruits</Title>
     </Head>
     <h1 class="text-center mt-20 uppercase">Fruits</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
+    <div v-if="data" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-10">
       <UICard v-for="product in data" :key="product.id">
         <div class="grid grid-cols-1">
           <UIImage :imgSrc="product.img" :imgName="product.name" className="h-80" />
@@ -17,10 +17,11 @@
         </div>
       </UICard>
     </div>
+    <section v-else class="flex justify-center items-center mt-10"><UILoader /></section>
   </div>
 </template>
 <script setup lang="ts">
 import { IFruit } from '@/interfaces/fruits';
 
-const { data } = await useFetch<IFruit[]>('/api/fruits');
+const { data } = await useFetch<IFruit[]>('/api/fruits', { lazy: true });
 </script>
